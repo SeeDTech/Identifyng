5f28a970594380771562c0380640c617a686f30a
embash <bashir.ibrahim@stetis.com>
2018-11-26 15:18:02 +0100

    added arrow burron to requirement page

--- src/components/buttons/Butons.js
+++ src/components/buttons/Butons.js
@@ -1,52 +1,57 @@
-import { TouchableOpacity,View,Text, StyleSheet } from 'react-native'
+import { TouchableOpacity, Platform, View, Text, StyleSheet } from 'react-native'
 import React, { Component } from 'react';
-import Icon from 'react-native-vector-icons/Ionicons'
-import { styles, buttons } from './styles'; 
+import Icon from '../Icon'
+import { styles, buttons } from './styles';
 
 
-export const TransparentButton=(props)=>{
-    handleOnPress = (e)=>{
+export const TransparentButton = (props) => {
+    handleOnPress = (e) => {
         props.onPress()
     }
-    const {title}=props;
-    return(
+    const { title } = props;
+    return (
         <View style={styles.container}>
-                <TouchableOpacity  onPress={handleOnPress} style={buttons.transparent}
-                >
-                    <Text style={buttons.greenText} >{title}</Text>
-                </TouchableOpacity>
-            </View>
+            <TouchableOpacity onPress={handleOnPress} style={buttons.transparent}
+            >
+                <Text style={buttons.greenText} >{title}</Text>
+            </TouchableOpacity>
+        </View>
     )
 }
 
-export const Button=(props)=>{
-    handleOnPress = (e)=>{
+export const Button = (props) => {
+    handleOnPress = (e) => {
         props.onPress();
     }
-    const {title}=props;
-    return(
+    const { title } = props;
+    return (
         <View style={styles.container}>
-                <TouchableOpacity  onPress={handleOnPress} style={buttons.primary}
-                >
-                    <Text style={buttons.whitetext} >{title}</Text>
-                </TouchableOpacity>
-            </View>
+            <TouchableOpacity onPress={handleOnPress} style={buttons.primary}
+            >
+                <Text style={buttons.whitetext} >{title}</Text>
+            </TouchableOpacity>
+        </View>
     )
 }
 
-export const NextButton=(props)=>{
-    handleOnPress = (e)=>{
+export const NextButton = (props) => {
+    handleOnPress = (e) => {
         props.onPress();
     }
-    const {title}=props;
-    
-    return(
+    const { title } = props;
+
+    return (
         <View style={styles.container}>
-                <TouchableOpacity  onPress={handleOnPress} style={buttons.circle}
-                >
-                    <Text style={buttons.whitetext}>{title}</Text>
-                </TouchableOpacity>
-            </View>
+            <TouchableOpacity onPress={handleOnPress} style={buttons.circle}
+            >
+                
+                <Icon style={{marginTop:14}}
+                    name="arrow-forward"
+                    color="#fff"
+                    size={45}
+                />
+            </TouchableOpacity>
+        </View>
     )
 }
 
